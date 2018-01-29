550013f264b353b1c3135e19648094d0ee6c8a41
Joey Pereira <joey@pereira.io>
2018-01-29 12:14:25 -0500

    parser: Fix reduction bug and add comments

--- src/orangejoos/parser.cr
+++ src/orangejoos/parser.cr
@@ -44,6 +44,10 @@ class Parser
         @input.shift
       end
 
+      # Get the lookahead string of the parse token. It is one of:
+      # - Literal (denoted with LEXEME(LiteralType))
+      # - Terminal token name. e.g. a keyword, "else", or an operator "+"
+      # - A parse node, resulting from a reduce step of parsing.
       if lookahead.is_a?(ParseNode)
         lookahead_str = lookahead.name
       elsif lookahead.is_a?(Lexeme)
@@ -62,6 +66,18 @@ class Parser
       end
 
       action = @table.get_next_action(@state, lookahead_str)
+      # Do either a reduce or a shift.
+      #
+      # For shift: take the lookahead and "read" it and then put it
+      # on the stack. We transition to the next state denoted by the
+      # lookahead.
+      #
+      # For reduce: push the lookahead back onto the unread input. Then
+      # look up the reduction rule and reduce the amount of RHS tokens.
+      # Finally, consider the newly produced LHS as the lookahead to do
+      # the next state transition. The state will be the state of the
+      # most recently popped stack element.
+      #
       # puts "For {#{@state}, \"#{lookahead.to_s}\"} got action=#{action.to_s}"
       if action.typ == ActionType::Shift
         @stack.push(lookahead)
@@ -76,7 +92,6 @@ class Parser
         # reducing items off the stack.
         tokens = (0...rule.reduce_size).map { |tree| @state = @state_stack.pop; @stack.pop }
         node = ParseNode.new(rule.lhs, tokens)
-        @stack.push(node)
 
         # puts "Rule ##{action.state}, reduce size #{rule.reduce_size}: #{rule.to_s}"
         # puts "Tokens #{tokens}"
