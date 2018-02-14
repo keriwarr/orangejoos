086ba7ae699be302615a31f35df999b6aa186d1a
Joey Pereira <joey@pereira.io>
2018-02-14 14:58:48 -0500

    prevent long, double, float keywords
    Because these types are not supported in Joos1W, these were not reserved
    words but because of the nature of Joos1W being a subset of Java, we
    still need to prevent programs from using these keywords.

--- src/orangejoos/lexeme.cr
+++ src/orangejoos/lexeme.cr
@@ -113,7 +113,10 @@ KEYWORDS = Set{
   # Other keywords that are not used in Joos1W but are forbidden
   "goto",
   "synchronized",
-  "volatile"
+  "volatile",
+  "float",
+  "double",
+  "long",
 }
 
 # A ParseNode is an abstract type for nodes that are operated on during
