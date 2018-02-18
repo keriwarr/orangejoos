b1d739f1c79a77e790e04fa73515f7e0cc026e65
Keri Warr <keri@warr.ca>
2018-02-18 15:34:07 -0500

    Use pub test cases in tester

--- tester.sh
+++ tester.sh
@@ -46,6 +46,170 @@ for file in $PASS_FILES; do
   fi
 done
 
+regex="^\/\/ (([A-Z_0-9]+)\: ?)?(([A-Z_0-9]+,)*[A-Z_0-9]+)$"
+
+for filename in pub/assignment_testcases/a1/*.java; do
+  should_pass=true;
+  if [[ $(basename $filename) == Je* ]]; then
+    should_pass=false;
+  fi
+
+  while IFS='' read -r line || [[ -n "$line" ]]; do
+    if [[ $line == \/\/\ * ]]; then
+      if [[ $line =~ $regex ]]; then
+        language="${BASH_REMATCH[2]}"
+        descriptorlist="${BASH_REMATCH[3]}"
+        IFS=', ' read -r -a descriptors <<< "$descriptorlist"
+
+        if [ -z "$language" ]; then
+          :
+        elif [[ $language == "JOOS1" ]]; then
+          :
+        elif [[ $language == "JOOS2" ]]; then
+          :
+        elif [[ $language == "JAVAC" ]]; then
+          break
+        else
+          break
+        fi
+
+        # echo $filename
+        for index in "${!descriptors[@]}"
+        do
+          case "${descriptors[index]}" in
+            PARSER_WEEDER)
+              RESULT=$(./joosc $filename -s weed >/dev/null 2>/dev/null)
+              result=$?
+              if $should_pass; then
+                if [[ $result = 42 ]]; then
+                  echo "== ${RED}FAIL${NC}: ${filename}"
+                  bad_fail=$((bad_fail + 1))
+                elif [[ $result = 0 ]]; then
+                  echo "== ${GREEN}PASS${NC}: ${filename}"
+                  correct_pass=$((correct_pass + 1))
+                else
+                  echo "== ${RED}ERROR${NC}: ${filename}"
+                  errors=$((errors + 1))
+                fi
+              else
+                if [[ $result = 42 ]]; then
+                  echo "== ${GREEN}FAIL${NC}: ${filename}"
+                  correct_fail=$((correct_fail + 1))
+                elif [[ $result = 0 ]]; then
+                  echo "== ${RED}PASS${NC}: ${filename}"
+                  bad_pass=$((bad_pass + 1))
+                else
+                  echo "== ${RED}ERROR${NC}: ${filename}"
+                  errors=$((errors + 1))
+                fi
+              fi
+              ;;
+            CODE_GENERATION)
+              ;;
+            TYPE_CHECKING)
+              ;;
+            ENVIRONMENTS)
+              ;;
+            HIERARCHY)
+              ;;
+            RESOURCES)
+              ;;
+            REACHABILITY)
+              ;;
+            JOOS1_STATIC_FIELD_DECLARATION)
+              ;;
+            PARSER_EXCEPTION)
+              ;;
+            DISAMBIGUATION)
+              ;;
+            JOOS1_THIS_CALL)
+              ;;
+            CIRCULAR_CONSTRUCTOR_INVOCATION)
+              ;;
+            JOOS1_MULTI_ARRAY)
+              ;;
+            ASSIGN_TYPE)
+              ;;
+            AMBIGUOUS_OVERLOADING)
+              ;;
+            JOOS1_INC_DEC)
+              ;;
+            ASSIGN_TO_ARRAY_LENGTH)
+              ;;
+            JOOS1_EXPLICIT_SUPER_CALL)
+              ;;
+            THIS_BEFORE_SUPER_CALL)
+              ;;
+            NO_MATCHING_CONSTRUCTOR_FOUND)
+              ;;
+            JOOS1_THROW)
+              ;;
+            ILLEGAL_THROWS)
+              ;;
+            UNREACHABLE_STATEMENT)
+              ;;
+            ABSTRACT_FINAL_CLASS)
+              ;;
+            ABSTRACT_METHOD_FINAL_OR_STATIC)
+              ;;
+            ABSTRACT_METHOD_BODY)
+              ;;
+            SYNTAX_ERROR)
+              ;;
+            INVALID_SOURCE_FILE_NAME)
+              ;;
+            LEXER_EXCEPTION)
+              ;;
+            JOOS1_FINAL_FIELD_DECLARATION)
+              ;;
+            MISSING_FINAL_FIELD_INITIALIZER)
+              ;;
+            INVALID_INSTANCEOF)
+              ;;
+            VOID_TYPE_NOT_RETURN_TYPE)
+              ;;
+            VOID_TYPE_INSTANCEOF)
+              ;;
+            INVALID_INTEGER)
+              ;;
+            JOOS1_INTERFACE)
+              ;;
+            INTERFACE_CONSTRUCTOR)
+              ;;
+            INTERFACE_FIELD)
+              ;;
+            STATIC_OR_FINAL_INTERFACE_METHOD)
+              ;;
+            INTERFACE_METHOD_WITH_BODY)
+              ;;
+            NON_ABSTRACT_METHOD_BODY)
+              ;;
+            STATIC_FINAL_METHOD)
+              ;;
+            SUPER_CALL_NOT_FIRST_STATEMENT)
+              ;;
+            THIS_CALL_NOT_FIRST_STATEMENT)
+              ;;
+            VOID_TYPE_ARRAY)
+              ;;
+            VOID_TYPE_CAST)
+              ;;
+            VOID_TYPE_FIELD)
+              ;;
+            VOID_TYPE_VARIABLE)
+              ;;
+            *)
+              ;;
+          esac
+        done
+      fi
+    else
+      break
+    fi
+  done < $filename
+done
+
+
 
 echo "=== RESULTS ==="
 echo "Correct: $((correct_pass + correct_fail))"
