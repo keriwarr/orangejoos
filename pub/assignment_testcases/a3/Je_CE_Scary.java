977aa960e7af50c6bf748743679273fafa4ea5de
Joey Pereira <joey@pereira.io>
2018-03-12 23:52:18 -0400

    modify test for parse-tree notes

--- pub/assignment_testcases/a3/Je_CE_Scary.java
+++ pub/assignment_testcases/a3/Je_CE_Scary.java
@@ -1,6 +1,12 @@
 public class Je_CE_Scary {
+  public Je_CE_Scary() {}
+
   public void foo() {
     int x = 1;
-    x = hah.haha.Za.length;
+    x = hah.haha.Za.length; // Parses as QualifiedName.
+    x = hah.Method().length; // Parses as FieldAccess.
+    x = hah.Method().length.lala; // Parses as FieldAccess.
+    x = hah.length; // Parses as QualifiedName.
+    x = (new String()).length; // Parses as FieldAccess.
   }
 }
