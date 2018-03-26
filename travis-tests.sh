9b100230b43463da987388bb12512211b6a56d4e
Keri Warr <keri@warr.ca>
2018-03-26 18:56:38 -0400

    Attempt to fix checkout

--- travis-tests.sh
+++ travis-tests.sh
@@ -4,7 +4,7 @@ set -v
 
 ./tester.sh
 mv failed_tests.tmp failed_tests_pr.tmp
-git checkout origin/master
+git checkout remotes/origin/master
 ./tester.sh
 diff failed_tests.tmp failed_tests_pr.tmp | grep "✗"
 if [[ $? = 0 ]]; then
