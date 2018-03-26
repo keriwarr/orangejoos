38d0ebaba07a98a7f575038e7814d90247731355
Keri Warr <keri@warr.ca>
2018-03-26 19:39:42 -0400

    Attempt to fix checkout

--- travis-tests.sh
+++ travis-tests.sh
@@ -4,8 +4,7 @@ set -v
 
 ./tester.sh
 mv failed_tests.tmp failed_tests_pr.tmp
-git fetch origin
-git checkout origin/master
+git checkout $(git ls-remote origin | grep "refs/heads/master" | awk '{ print $1 }')
 ./tester.sh
 diff failed_tests.tmp failed_tests_pr.tmp | grep "✗"
 if [[ $? = 0 ]]; then
