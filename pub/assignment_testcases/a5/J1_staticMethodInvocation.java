567286ce51734feb62708f81927c467ee5faa4e3
Joey Pereira <joey@pereira.io>
2018-03-31 21:30:57 -0400

    rename custom test

--- pub/assignment_testcases/a5/J1_staticMethodInvocation.java
+++ pub/assignment_testcases/a5/J1_staticMethodInvocation.java
@@ -1,17 +1,16 @@
 // DISAMBIGUATION,TYPE_CHECKING,CODE_GENERATION
 public class J1_staticMethodInvocation {
-    public J1_staticMethodInvocation() {
+
+    public static int bar() {
+	return 123;
     }
 
-    public static int baz(int a, int b, int c) {
-        return a * a - b * c;
-    }
+    public J1_staticMethodInvocation() {}
 
-    public static int bar(int a, int b) {
-        return J1_staticMethodInvocation.baz(a + 2, b, b) + 43;
-    }
 
     public static int test() {
-        return J1_staticMethodInvocation.bar(10, 8);
+	return J1_staticMethodInvocation.bar();
     }
+
+
 }
